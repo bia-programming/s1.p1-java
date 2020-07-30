d733669b1881bc5a692d335f3c7c66e54e939376
bia-programming <bia.pasca@yahoo.com>
2020-07-30 15:40:55 +0300

    Problema reactualizata

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,15 @@
 package com.company;
 import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)  throws IOException{
-	    int nr;
-        nr = (int) System.in.read();
+        final Scanner myInput = new Scanner(System.in);
+        final int nr = myInput.nextInt();
 	    numar(nr);
     }
-    static void numar(int nr){
+    static void numar(final int nr){
         if(nr%2==0)
             System.out.println("Nr este par");
         else
